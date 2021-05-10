dc06cdb7f5a0c84cfb90492e66a49a2d2e9a2c4e
Nuno <up201705462@fe.up.pt>
2021-05-10 19:01:21 +0100

    Top até ao inf mais 1

--- exercicio-nn/sheet7_exe1.m
+++ exercicio-nn/sheet7_exe1.m
@@ -12,8 +12,9 @@ xa_t=xa;
 xb_t=xb;
 y_t = yd;
 vetor_erro = y_t;
+R = yd;
 
-erro_desejado = ones(1,200).*0.15
+erro_desejado = ones(1,200).*0.15;
 
 cnt = 1;
 
@@ -27,11 +28,11 @@ sizeA = [3 1000];
 sizeB = [2 1000];
 fileID = fopen('testInput10A.txt', 'r');
 
-[A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lê tudo que é para treino
+[A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lï¿½ tudo que ï¿½ para treino
 
 B=[A(1,length(A)); A(2,length(A))];
 [B1, count] = fscanf(fileID, '%f,%f', sizeB);
-B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lê tudo que é para testar
+B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lï¿½ tudo que ï¿½ para testar
 fclose(fileID);
 A=A';
 B=B';
@@ -46,6 +47,11 @@ for i=1:1:size(A, 1) %retira valores depois de encontrar 0
        xa(i) = A(i,1);
        xb(i) = A(i,2);
        yd(i) = A(i,3); %output desejado
+       if(A(i,3) == 1)
+           R(i) = 1; %plot red
+       else
+           R(i) = 0;
+       end
    end
 end
 
@@ -55,11 +61,22 @@ for i=1:1:size(B, 1) %retira valores depois de encontrar 0
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 xa = xa(1:1:s-1); %truncar 
 xb = xb(1:1:s-1);
 xa_t = xa_t(1:1:length(B));
 xb_t = xb_t(1:1:length(B));
+R = R(1:1:s-1);
+vetor_erro = vetor_erro(1:1:s-1);
+figure(1)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    title('Plot dos pontos (xa,xb) lidos do ficheiro')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
+    %xb.*R;
+
 %%%%%%%%%%%%%%%%normalizar dados de input%%%%%%%%%%%%%%%%%%%
 maximo = max([xa,xb]);
 minimo = min([xa,xb]);
@@ -68,7 +85,17 @@ xb = (xb - minimo)./(maximo-minimo);
 xa_t = (xa_t - minimo)./(maximo-minimo);
 xb_t = (xb_t - minimo)./(maximo-minimo);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+    
+figure(2)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    plot(xa,2.2594*xa-0.54); %linha
+    title('Plot dos pontos (xa,xb) normalizados')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
+    %xb.*R;
 %%%%%%%%%%%%%%%%sum e func de ativ%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 somatorio = xa.*w(1) + xb.*w(2);
