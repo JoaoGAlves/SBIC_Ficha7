715efff88936e71bc79f8de52b78c1843ca0ba01
Nuno <up201705462@fe.up.pt>
2021-05-11 01:22:44 +0100

    Implementação mais polida do problema de modo a trocar de ficheiro rapid

--- exercicio-nn/sheet7_exe1_Simple.m
+++ exercicio-nn/sheet7_exe1_Simple.m
@@ -5,15 +5,14 @@ syms x;
 xa = zeros(1,1000);
 xb = xa; %inicializar xa e xb
 yd = xb;
-somatorio= yd;
-somatorio_t = yd;
 y = yd;
 xa_t=xa;
 xb_t=xb;
 y_t = yd;
 vetor_erro = y_t;
+R = yd;
 
-erro_desejado = ones(1,200).*0.15
+erro_desejado = ones(1,200).*0.15;
 
 cnt = 1;
 
@@ -26,14 +25,32 @@ theta = 1;
 %%%%%%%%%%%%%%%%ler txt%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sizeA = [3 1000];
 sizeB = [2 1000];
-fileID = fopen('testInput10A.txt', 'r');
+sizeC = [1 1000];
 
-[A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lê tudo que é para treino
+f = 3;
 
-B=[A(1,length(A)); A(2,length(A))];
-[B1, count] = fscanf(fileID, '%f,%f', sizeB);
-B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lê tudo que é para testar
+switch f
+    case 1
+        stringInputF = 'testInput10A.txt';
+        stringOutputF = 'testOutput10A.txt';
+    case 2
+        stringInputF = 'testInput10B.txt';
+        stringOutputF = 'testOutput10B.txt';
+    case 3
+        stringInputF = 'testInput10C.txt';
+        stringOutputF = 'testOutput10C.txt';
+end
+fileID = fopen(stringInputF, 'r');
+    [A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lê tudo que é para treino
+    B=[A(1,length(A)); A(2,length(A))];
+    [B1, count] = fscanf(fileID, '%f,%f', sizeB);
+    B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lê tudo que é para testar
 fclose(fileID);
+
+fileID = fopen(stringOutputF, 'r');
+    [C,count] = fscanf(fileID, '%d', sizeC);
+fclose(fileID);
+
 A=A';
 B=B';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -47,6 +64,11 @@ for i=1:1:size(A, 1) %retira valores depois de encontrar 0
        xa(i) = A(i,1);
        xb(i) = A(i,2);
        yd(i) = A(i,3); %output desejado
+       if(A(i,3) == 1)
+           R(i) = 1; %plot blue (1)
+       else
+           R(i) = 0; %plot  red (-1)
+       end
    end
 end
 
@@ -61,6 +83,15 @@ xa = xa(1:1:s-1); %truncar
 xb = xb(1:1:s-1);
 xa_t = xa_t(1:1:length(B));
 xb_t = xb_t(1:1:length(B));
+R = R(1:1:s-1);
+    figure(1)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    title('Plot dos pontos (xa,xb) lidos do ficheiro')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
 %%%%%%%%%%%%%%%%normalizar dados de input%%%%%%%%%%%%%%%%%%%
 maximo = max([xa,xb]);
 minimo = min([xa,xb]);
@@ -70,26 +101,21 @@ xa_t = (xa_t - minimo)./(maximo-minimo);
 xb_t = (xb_t - minimo)./(maximo-minimo);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    figure(2)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    %plot(xa,2.2594*xa-0.54); %linha
+    title('Plot dos pontos (xa,xb) normalizados')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
+    
 %%%%%%%%%%%%%%%%sum e func de ativ%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-somatorio = xa.*w(1) + xb.*w(2);
-somatorio = somatorio(1:1:s-1);
 y=y(1:1:s-1);
 yd=yd(1:1:s-1);
-
-% for i=1:1:s-1  
-%     y(i) = sign(somatorio(i) - theta);
-% end
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% %%%%%%%%%%%%%%%%calculo erro e delta rule%%%%%%%%%%%%%%%%%%%
-% 
-%  erro  = (yd - y);
-% 
-%  delta1 = alpha*erro*xa';
-%  delta2 = alpha*erro*xb';
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%treino%%%%%%%%%%%%%%%%%%% 
 while 1 
@@ -102,18 +128,17 @@ while 1
     w(3) = w(3) + alpha*(yd(j)-percepton_output)*theta;
     
     end
-    y = sign(xa.*w(1)+xb.*w(2)+theta*w(3))
+    y = sign(xa.*w(1)+xb.*w(2)+theta*w(3));
     if(yd-y == 0)
         break
     end
 end
 
 
-%%plot(iteracao,vetor_erro);
-
 %%%%%%%%%%%%%%%%%%%%%%%testar%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 y_t=y_t(1:1:length(B));
 y_t = sign(xa_t.*w(1) + xb_t.*w(2) + theta*(w(3)));
+comparacao_entre_resultado_e_target = C - y_t;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
--- exercicio-nn/sheet7_exe1_Simple_tanh.m
+++ exercicio-nn/sheet7_exe1_Simple_tanh.m
@@ -5,15 +5,13 @@ syms x;
 xa = zeros(1,1000);
 xb = xa; %inicializar xa e xb
 yd = xb;
-somatorio= yd;
-somatorio_t = yd;
 y = yd;
 xa_t=xa;
 xb_t=xb;
 y_t = yd;
 vetor_erro = y_t;
 
-erro_desejado = ones(1,200).*0.15
+erro_desejado = ones(1,1000).*0.15;
 
 cnt = 1;
 
@@ -25,14 +23,30 @@ theta = 1;
 
 %%%%%%%%%%%%%%%%ler txt%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sizeA = [3 1000];
-sizeB = [2 1000];
-fileID = fopen('testInput10A.txt', 'r');
+sizeB = [2 1000];sizeC = [1 1000];
 
-[A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lê tudo que é para treino
+f = 1;
 
-B=[A(1,length(A)); A(2,length(A))];
-[B1, count] = fscanf(fileID, '%f,%f', sizeB);
-B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lê tudo que é para testar
+switch f
+    case 1
+        stringInputF = 'testInput10A.txt';
+        stringOutputF = 'testOutput10A.txt';
+    case 2
+        stringInputF = 'testInput10B.txt';
+        stringOutputF = 'testOutput10B.txt';
+    case 3
+        stringInputF = 'testInput10C.txt';
+        stringOutputF = 'testOutput10C.txt';
+end
+fileID = fopen(stringInputF, 'r');
+    [A,count] = fscanf(fileID, '%f,%f,%d', sizeA); %lê tudo que é para treino
+    B=[A(1,length(A)); A(2,length(A))];
+    [B1, count] = fscanf(fileID, '%f,%f', sizeB);
+    B=[B(1,1), B1(1,:);B(2,1), B1(2,:)]; %lê tudo que é para testar
+fclose(fileID);
+
+fileID = fopen(stringOutputF, 'r');
+    [C,count] = fscanf(fileID, '%d', sizeC);
 fclose(fileID);
 A=A';
 B=B';
@@ -47,6 +61,11 @@ for i=1:1:size(A, 1) %retira valores depois de encontrar 0
        xa(i) = A(i,1);
        xb(i) = A(i,2);
        yd(i) = A(i,3); %output desejado
+       if(A(i,3) == 1)
+           R(i) = 1; %plot blue (1)
+       else
+           R(i) = 0; %plot  red (-1)
+       end
    end
 end
 
@@ -61,6 +80,15 @@ xa = xa(1:1:s-1); %truncar
 xb = xb(1:1:s-1);
 xa_t = xa_t(1:1:length(B));
 xb_t = xb_t(1:1:length(B));
+R = R(1:1:s-1);
+    figure(1)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    title('Plot dos pontos (xa,xb) lidos do ficheiro')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
 %%%%%%%%%%%%%%%%normalizar dados de input%%%%%%%%%%%%%%%%%%%
 maximo = max([xa,xb]);
 minimo = min([xa,xb]);
@@ -70,28 +98,25 @@ xa_t = (xa_t - minimo)./(maximo-minimo);
 xb_t = (xb_t - minimo)./(maximo-minimo);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    figure(2)
+    hold on
+    plot(xa.*R,xb.*R,'ro'); %red = 1
+    plot(xa.*(1-R),xb.*(1-R),'bo'); %blue = -1
+    %plot(xa,2.2594*xa-0.54); %linha
+    title('Plot dos pontos (xa,xb) normalizados')
+    xlabel('xa')
+    ylabel('xb')
+    hold off
+    
 %%%%%%%%%%%%%%%%sum e func de ativ%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-somatorio = xa.*w(1) + xb.*w(2);
-somatorio = somatorio(1:1:s-1);
 y=y(1:1:s-1);
 yd=yd(1:1:s-1);
 
-% for i=1:1:s-1  
-%     y(i) = sign(somatorio(i) - theta);
-% end
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% %%%%%%%%%%%%%%%%calculo erro e delta rule%%%%%%%%%%%%%%%%%%%
-% 
-%  erro  = (yd - y);
-% 
-%  delta1 = alpha*erro*xa';
-%  delta2 = alpha*erro*xb';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%treino%%%%%%%%%%%%%%%%%%% 
+erro_desejado = erro_desejado(1:1:s-1);
 while 1 
     
     for j=1:1:s-1 
@@ -108,12 +133,10 @@ while 1
     end
 end
 
-
-%%plot(iteracao,vetor_erro);
-
 %---------------------------testar--------------------------%
 y_t=y_t(1:1:length(B));
 y_t = tanh(10*(xa_t.*w(1) + xb_t.*w(2) + theta*(w(3))));
+comparacao_entre_resultado_e_target = C - y_t;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
